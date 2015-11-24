ac36c4fb46e341cdedde0a5413f190c9897ac328
Vlad Kopachinsky <vlad.kopachinsky@gmail.com>
2015-11-24 14:22:40 +0300

    add url as data

--- src/Incoding/MvcContrib/Incoding Controls/IncDropDownControl.cs
+++ src/Incoding/MvcContrib/Incoding Controls/IncDropDownControl.cs
@@ -15,6 +15,72 @@ namespace Incoding.MvcContrib
 
     public class IncDropDownControl<TModel, TProperty> : IncControlBase
     {
+        #region Constructors
+
+        public IncDropDownControl(HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> property)
+        {
+            this.htmlHelper = htmlHelper;
+            this.property = property;
+            Data = new SelectList(new string[0]);
+            Template = IncodingHtmlHelper.DropDownTemplateId;
+            InitBind = JqueryBind.InitIncoding;
+            Optional = new IncSelectOptional();
+        }
+
+        #endregion
+
+        public override MvcHtmlString ToHtmlString()
+        {
+            bool isAjax = !string.IsNullOrWhiteSpace(Url);
+
+            bool isIml = OnInit != null ||
+                         OnChange != null ||
+                         OnEvent != null;
+
+            if (isAjax || isIml)
+            {
+                var meta = isAjax ? htmlHelper.When(InitBind).Do().Ajax(options =>
+                                                                        {
+                                                                            options.Url = Url;
+                                                                            options.Type = HttpVerbs.Get;
+                                                                        })
+                                   : htmlHelper.When(InitBind).Do().Direct();
+                attributes = meta.OnSuccess(dsl =>
+                                            {
+                                                if (isAjax)
+                                                {
+                                                    dsl.Self().Insert.WithTemplate(Template).Html();
+                                                    foreach (var vm in Optional.Values)
+                                                    {
+                                                        var option = new TagBuilder(HtmlTag.Option.ToStringLower());
+                                                        option.SetInnerText(vm.Text);
+                                                        option.MergeAttribute(HtmlAttribute.Value.ToStringLower(), vm.Value);
+                                                        dsl.Self().JQuery.Dom.Use(new MvcHtmlString(option.ToString()).ToHtmlString()).Prepend();
+                                                    }
+
+                                                    var selected = ModelMetadata.FromLambdaExpression(property, htmlHelper.ViewData).Model;
+                                                    if (selected != null)
+                                                        dsl.Self().JQuery.Attributes.Val(selected);
+                                                }
+
+                                                OnInit.Do(action => action(dsl));
+                                                OnEvent.Do(action => action(dsl));
+                                            })
+                                 .When(JqueryBind.Change)
+                                 .OnSuccess(dsl =>
+                                            {
+                                                OnChange.Do(action => action(dsl));
+                                                OnEvent.Do(action => action(dsl));
+                                            })
+                                 .AsHtmlAttributes(attributes);
+            }
+
+            if (!isAjax)
+                Data.AddOptional(Optional.Values);
+
+            return htmlHelper.DropDownListFor(property, Data.AsSelectList, attributes);
+        }
+
         #region Fields
 
         readonly HtmlHelper<TModel> htmlHelper;
@@ -25,71 +91,66 @@ namespace Incoding.MvcContrib
 
         #endregion
 
-        #region Constructors
-
-        public IncDropDownControl(HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> property)
-        {
-            this.htmlHelper = htmlHelper;
-            this.property = property;
-            Data = new SelectList(new string[0]);
-            Template = IncodingHtmlHelper.DropDownTemplateId;
-            InitBind = JqueryBind.InitIncoding;
-        }
-
-        #endregion
-
         #region Properties
 
         public JqueryBind InitBind { get; set; }
 
         public JqueryAjaxOptions Options { get { return options; } set { options = value; } }
 
-        public string Url { get { return Options.Url; } set { Options.Url = value; } }
+        public string Url { get { return Data.Url ?? Options.Url; } set { Options.Url = value; } }
 
         public IncSelectList Data { get; set; }
 
         public Selector Template { get; set; }
 
-        public IEnumerable<KeyValueVm> Optional { get; set; }
+        public IncSelectOptional Optional { get; set; }
 
         #endregion
 
         #region Nested classes
 
+        public class IncSelectOptional
+        {
+            private readonly List<KeyValueVm> values = new List<KeyValueVm>();
+
+            public IncSelectOptional(string value)
+                    : this(new KeyValueVm(value)) { }
+
+            public IncSelectOptional(KeyValueVm value)
+                    : this(new[] { value }) { }
+
+            public IncSelectOptional(IEnumerable<KeyValueVm> value)
+            {
+                this.values = value.ToList();
+            }
+
+            public IncSelectOptional() { }
+
+            public List<KeyValueVm> Values { get { return this.values; } }
+
+            public static implicit operator IncSelectOptional(List<KeyValueVm> s)
+            {
+                return new IncSelectOptional(s);
+            }
+
+            public static implicit operator IncSelectOptional(KeyValueVm[] s)
+            {
+                return new IncSelectOptional(s);
+            }
+
+            public static implicit operator IncSelectOptional(string s)
+            {
+                return new IncSelectOptional(s);
+            }
+
+            public static implicit operator IncSelectOptional(KeyValueVm s)
+            {
+                return new IncSelectOptional(s);
+            }
+        }
+
         public class IncSelectList
         {
-            #region Fields
-
-            readonly IList<KeyValueVm> keyValueVms;
-
-            #endregion
-
-            #region Constructors
-
-            public IncSelectList(IEnumerable<KeyValueVm> keyValueVms)
-            {
-                this.keyValueVms = keyValueVms.ToList();
-            }
-
-            public IncSelectList(SelectList asSelectList)
-            {
-                keyValueVms = new List<KeyValueVm>();
-                foreach (var item in asSelectList.Items)
-                {
-                    keyValueVms.Add(item.GetType().IsTypicalType()
-                                            ? new KeyValueVm(item.ToString())
-                                            : new KeyValueVm(item.TryGetValue(asSelectList.DataValueField), item.TryGetValue(asSelectList.DataTextField).With(r => r.ToString())));
-                }
-            }
-
-            #endregion
-
-            #region Properties
-
-            public SelectList AsSelectList { get { return new SelectList(keyValueVms, "Value", "Text"); } }
-
-            #endregion
-
             #region Api Methods
 
             public void AddOptional(IList<KeyValueVm> defaults)
@@ -119,60 +180,54 @@ namespace Incoding.MvcContrib
             {
                 return new IncSelectList(s);
             }
+
+            public static implicit operator IncSelectList(string url)
+            {
+                return new IncSelectList(url);
+            }
+
+            #region Fields
+
+            readonly IList<KeyValueVm> keyValueVms = new List<KeyValueVm>();
+
+            private readonly string url;
+
+            #endregion
+
+            #region Properties
+
+            public SelectList AsSelectList { get { return new SelectList(keyValueVms, "Value", "Text"); } }
+
+            public string Url { get { return this.url; } }
+
+            #endregion
+
+            #region Constructors
+
+            public IncSelectList(IEnumerable<KeyValueVm> keyValueVms)
+            {
+                this.keyValueVms = keyValueVms.ToList();
+            }
+
+            public IncSelectList(SelectList asSelectList)
+            {
+                keyValueVms = new List<KeyValueVm>();
+                foreach (var item in asSelectList.Items)
+                {
+                    keyValueVms.Add(item.GetType().IsTypicalType()
+                                            ? new KeyValueVm(item.ToString())
+                                            : new KeyValueVm(item.TryGetValue(asSelectList.DataValueField), item.TryGetValue(asSelectList.DataTextField).With(r => r.ToString())));
+                }
+            }
+
+            private IncSelectList(string asSelectList)
+            {
+                this.url = asSelectList;
+            }
+
+            #endregion
         }
 
         #endregion
-
-        public override MvcHtmlString ToHtmlString()
-        {
-            bool isAjax = !string.IsNullOrWhiteSpace(Url);
-
-            bool isIml = OnInit != null ||
-                         OnChange != null ||
-                         OnEvent != null;
-
-            if (isAjax || isIml)
-            {
-                var meta = isAjax ? htmlHelper.When(InitBind).Do().Ajax(options =>
-                                                                        {
-                                                                            options.Url = Url;
-                                                                            options.Type = HttpVerbs.Get;
-                                                                        })
-                                   : htmlHelper.When(InitBind).Do().Direct();
-                attributes = meta.OnSuccess(dsl =>
-                                            {
-                                                if (isAjax)
-                                                {
-                                                    dsl.Self().Insert.WithTemplate(Template).Html();
-                                                    foreach (var vm in Optional.Recovery(new KeyValueVm[0]))
-                                                    {
-                                                        var option = new TagBuilder(HtmlTag.Option.ToStringLower());
-                                                        option.SetInnerText(vm.Text);
-                                                        option.MergeAttribute(HtmlAttribute.Value.ToStringLower(), vm.Value);
-                                                        dsl.Self().JQuery.Dom.Use(new MvcHtmlString(option.ToString()).ToHtmlString()).Prepend();
-                                                    }
-
-                                                    var selected = ModelMetadata.FromLambdaExpression(property, htmlHelper.ViewData).Model;
-                                                    if (selected != null)
-                                                        dsl.Self().JQuery.Attributes.Val(selected);
-                                                }
-
-                                                OnInit.Do(action => action(dsl));
-                                                OnEvent.Do(action => action(dsl));
-                                            })
-                                 .When(JqueryBind.Change)
-                                 .OnSuccess(dsl =>
-                                            {
-                                                OnChange.Do(action => action(dsl));
-                                                OnEvent.Do(action => action(dsl));
-                                            })
-                                 .AsHtmlAttributes(attributes);
-            }
-
-            if (!isAjax)
-                Data.AddOptional(Optional.Recovery(new KeyValueVm[0]).ToList());
-
-            return htmlHelper.DropDownListFor(property, Data.AsSelectList, attributes);
-        }
     }
 }
