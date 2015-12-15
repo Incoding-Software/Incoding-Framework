edab49eda352dd0e04bf701c89fdf399362c36e6
Vlad Kopachinsky <vlad.kopachinsky@gmail.com>
2015-12-15 14:48:34 +0300

    fixed bootstrap 2

--- src/Incoding/MvcContrib/Incoding Controls/IncHorizontalControl.cs
+++ src/Incoding/MvcContrib/Incoding Controls/IncHorizontalControl.cs
@@ -44,15 +44,22 @@ namespace Incoding.MvcContrib
             bool isV3orMore = IncodingHtmlHelper.BootstrapVersion == BootstrapOfVersion.v3;
             bool isStatic = Input.GetType().Name.Contains("IncStaticControl");
 
-            if (isV3orMore)
-                AddClass(B.Form_group);
+            
+            AddClass(isV3orMore ? B.Form_group.AsClass() : "control-group");
 
             Label.AddClass(B.Control_label);
             if (isV3orMore && isForDefClass(Label))
                 Label.AddClass(IncodingHtmlHelper.Def_Label_Class.AsClass());
 
+            if(!isV3orMore)
+                Control.AddClass("controls");
+
             if (string.IsNullOrWhiteSpace(Control.GetAttr(HtmlAttribute.Class)))
-                Control.AddClass(isV3orMore ? IncodingHtmlHelper.Def_Control_Class.AsClass() : isStatic ? string.Empty : "control-group");
+            {
+                Control.AddClass(isV3orMore
+                                         ? IncodingHtmlHelper.Def_Control_Class.AsClass()
+                                         : isStatic ? string.Empty : "control-group");
+            }
 
             var stringBuilder = new StringBuilder();
 
