027b62055b289b76b72e53035a6dc8a9304fdf01
Vlad Kopachinsky <vlad.kopachinsky@gmail.com>
2016-01-18 11:39:52 +0300

    closes #28: added inc html string

--- src/Incoding.UnitTest/MvcContribGroup/When_inc_html_string_cast.cs
+++ src/Incoding.UnitTest/MvcContribGroup/When_inc_html_string_cast.cs
@@ -1,14 +1,30 @@
 namespace Incoding.UnitTest.MvcContribGroup
 {
+    #region << Using >>
+
     using System;
     using System.Web.Mvc;
     using System.Web.WebPages;
     using Incoding.MvcContrib;
     using Machine.Specifications;
 
+    #endregion
+
     [Subject(typeof(IncSelectList))]
     public class When_inc_html_string_cast
     {
+        It should_be_cast_inc_to_mvc_string = () =>
+                                              {
+                                                  MvcHtmlString htmlString = new IncHtmlString(new MvcHtmlString("Optional"));
+                                                  htmlString.ToHtmlString().ShouldEqual("Optional");
+                                              };
+
+        It should_be_cast_inc_to_string = () =>
+                                          {
+                                              string htmlString = new IncHtmlString(new MvcHtmlString("Optional"));
+                                              htmlString.ShouldEqual("Optional");
+                                          };
+
         It should_be_cast_to_mvc_content_string = () =>
                                                   {
                                                       Func<object, HelperResult> converter = o => new HelperResult(writer => writer.WriteLine("<div>"));
