598d9145010a324014f3397b0d97510485ac3565
Vlad Kopachinsky <vlad.kopachinsky@gmail.com>
2016-02-10 08:59:11 +0300

    this closed #53 - fixed nuspec (added content, libs and etc)

--- src/Incoding.NuGet.MetaLanguage/tools/install.ps1
+++ src/Incoding.NuGet.MetaLanguage/tools/install.ps1
@@ -1,11 +1,1 @@
 ﻿# Runs every time a package is installed in a project
-
-param($installPath, $toolsPath, $package, $project)
-$file = $project.ProjectItems | ForEach-Object { $_.ProjectItems } | where { $_.Name -eq "Global.asax.cs" }
-if($file) {
-    $file.Open()
-    $file.Document.Activate()
-    $file.Document.Selection.StartOfDocument()
-	$file.Document.ReplaceText("Bootstrapper.Start();","")
-	$file.Document.ReplaceText("new DispatcherController(); // init routes","")    
-	}
