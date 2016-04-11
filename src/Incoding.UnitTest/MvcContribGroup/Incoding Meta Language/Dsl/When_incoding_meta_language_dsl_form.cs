1d164b5aa854058dbc39e6623e9cd378d019c333
Vlad Kopachinsky <vlad.kopachinsky@gmail.com>
2016-04-11 13:44:15 +0300

    remove form-control for checkbox on ForGroup

--- src/Incoding.UnitTest/MvcContribGroup/Incoding Meta Language/Dsl/When_incoding_meta_language_dsl_form.cs
+++ src/Incoding.UnitTest/MvcContribGroup/Incoding Meta Language/Dsl/When_incoding_meta_language_dsl_form.cs
@@ -12,33 +12,30 @@ namespace Incoding.UnitTest.MvcContribGroup
     [Subject(typeof(IncodingMetaCallbackFormApiDsl))]
     public class When_incoding_meta_language_dsl_form
     {
-        It should_be_clear = () => new IncodingMetaLanguageDsl(JqueryBind.Click)
-                                           .Do().Direct()
-                                           .OnSuccess(r => r.With(selector => selector.Id("id")).Core().Form.Clear())
+        It should_be_clear = () => new IncodingMetaLanguageDsl(JqueryBind.Click)                                           
+                                           .OnSuccess(r => r.With(selector => selector.Id("id")).Form.Clear())
                                            .GetExecutable<ExecutableForm>()
                                            .ShouldEqualData(new Dictionary<string, object> { { "method", "clear" } });
 
         It should_be_reset = () => new IncodingMetaLanguageDsl(JqueryBind.Click)
-                                           .Do().Direct()
+                                           
                                            .OnSuccess(r => r.With(selector => selector.Id("id")).Form.Reset())
                                            .GetExecutable<ExecutableForm>()
                                            .ShouldEqualData(new Dictionary<string, object> { { "method", "reset" } });
 
         It should_be_validation_parse = () => new IncodingMetaLanguageDsl(JqueryBind.Click)
-                                                      .Do().Direct()
-                                                      .OnSuccess(r => r.With(selector => selector.Id("id")).Core().Form.Validation.Parse())
+                                                      
+                                                      .OnSuccess(r => r.With(selector => selector.Id("id")).Form.Validation.Parse())
                                                       .GetExecutable<ExecutableValidationParse>()
                                                       .ShouldEqualData(new Dictionary<string, object>());
 
-        It should_be_validation_refresh = () => new IncodingMetaLanguageDsl(JqueryBind.Click)
-                                                        .Do().Direct()
-                                                        .OnSuccess(r => r.With(selector => selector.Id("id")).Core().Form.Validation.Refresh())
+        It should_be_validation_refresh = () => new IncodingMetaLanguageDsl(JqueryBind.Click)                                                        
+                                                        .OnSuccess(r => r.With(selector => selector.Id("id")).Form.Validation.Refresh())
                                                         .GetExecutable<ExecutableValidationRefresh>()
                                                         .ShouldEqualData(new Dictionary<string, object>());
 
-        It should_be_validation_refresh_with_prefix = () => new IncodingMetaLanguageDsl(JqueryBind.Click)
-                                                                    .Do().Direct()
-                                                                    .OnSuccess(r => r.With(selector => selector.Id("id")).Core().Form.Validation.Refresh(prefix: Pleasure.Generator.TheSameString()))
+        It should_be_validation_refresh_with_prefix = () => new IncodingMetaLanguageDsl(JqueryBind.Click)                                                                    
+                                                                    .OnSuccess(r => r.With(selector => selector.Id("id")).Form.Validation.Refresh(prefix: Pleasure.Generator.TheSameString()))
                                                                     .GetExecutable<ExecutableValidationRefresh>()
                                                                     .ShouldEqualData(new Dictionary<string, object>()
                                                                                      {
--- src/Incoding/MvcContrib/Incoding Controls/IncHorizontalControl.cs
+++ src/Incoding/MvcContrib/Incoding Controls/IncHorizontalControl.cs
@@ -45,14 +45,13 @@ namespace Incoding.MvcContrib
             bool isV3orMore = IncodingHtmlHelper.BootstrapVersion == BootstrapOfVersion.v3;
             bool isStatic = Input.GetType().Name.Contains("IncStaticControl");
 
-            
             AddClass(isV3orMore ? B.Form_group.ToLocalization() : "control-group");
 
             Label.AddClass(B.Control_label);
             if (isV3orMore && isForDefClass(Label))
                 Label.AddClass(IncodingHtmlHelper.Def_Label_Class.ToLocalization());
 
-            if(!isV3orMore)
+            if (!isV3orMore)
                 Control.AddClass("controls");
 
             if (string.IsNullOrWhiteSpace(Control.GetAttr(HtmlAttribute.Class)))
@@ -66,7 +65,7 @@ namespace Incoding.MvcContrib
 
             stringBuilder.Append(Label.ToHtmlString());
 
-            if (isV3orMore)
+            if (isV3orMore && !typeof(TInput).Name.Contains("IncCheckBoxControl"))
                 Input.AddClass(isStatic ? B.Form_static_control.ToLocalization() : B.Form_control.ToLocalization());
             Control.Content = Input.ToHtmlString();
 
